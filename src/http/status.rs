592ba82778e31f6606efea2f0e1291511e3e2ce9
Alice Pote <alice.writes.wrongs@gmail.com>
2024-01-19 18:36:52 -0500

    add formatters (basic for now)

--- src/http/status.rs
+++ src/http/status.rs
@@ -1,4 +1,15 @@
+use std::fmt;
+
 pub enum HttpStatus {
     Ok200,
     NotFound404,
+}
+
+impl fmt::Display for HttpStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HttpStatus::Ok200 => write!(f, "200 OK"),
+            HttpStatus::NotFound404 => write!(f, "404 NOT FOUND"),
+        }
+    }
 }
--- src/http/version.rs
+++ src/http/version.rs
@@ -1,4 +1,12 @@
+use std::fmt;
+
 #[derive(Clone, Debug)]
 pub enum HttpVersion {
     OneDotOne,
+}
+
+impl fmt::Display for HttpVersion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "HTTP/1.1")
+    }
 }
