873432d137425c5cfa4b2ac498f8e017ef1e5fa1
Alice Pote <alice.writes.wrongs@gmail.com>
2024-01-21 15:45:47 -0500

    add simple support for request a directory / index.html file

--- src/server.rs
+++ src/server.rs
@@ -22,9 +22,18 @@ pub fn serve_file(request: &Request, serve_path: &Path) -> Result<Response> {
     }
 }
 
-fn derive_file_path(serve_path: &Path, path: &str) -> Result<PathBuf> {
-    let mut serve_path = serve_path.to_path_buf();
+fn derive_file_path(serve_path: &Path, request_path: &str) -> Result<PathBuf> {
+    let mut file_path = serve_path.to_path_buf();
     // the `path` is from the HTTP request so it will always have a leading '/' character
-    serve_path.push(path.trim_start_matches('/'));
-    Ok(serve_path)
+    file_path.push(request_path.trim_start_matches('/'));
+    // TODO support other file types
+    //
+    // what happens if you request `/foo` and there's a file called `foo` in the directory?
+    //
+    // perhaps the server should only serve 'web-ish' file types, i.e. file-types have have defined
+    // MIME types (images, json, javascript, etc).
+    if !file_path.ends_with("index.html") {
+        file_path.push("index.html");
+    }
+    Ok(file_path)
 }
