c18216938524fa7ed6fafccced652ba405003b29
Alice Pote <alice.writes.wrongs@gmail.com>
2024-02-01 16:31:32 -0500

    refactor the server module
    change things around a little so that:
    
    - the server module exports a `Server` struct that does the stuff that
      was sort of inlined in `main.rs` before
    - the file serving stuff is now in a non-public submodule of the
      `server` module
    
    I want to do this both to keep things neat and because I need to add
    some code for handling SIGTERM and whatnot and I don't want to have
    `main.rs` get too cluttered.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 #![feature(thread_id_value)]
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
-use std::io::{prelude::*, BufReader};
-use std::net::{TcpListener, TcpStream};
-use std::path::Path;
-use std::thread;
-
-static SERVER_ADDRESS: &str = "127.0.0.1:8080";
 
 mod cli;
 mod http;
@@ -16,70 +10,14 @@ mod response;
 mod server;
 
 use cli::CLIArgs;
-use parser::parse_request;
-
-fn handle_stream(mut stream: TcpStream, path: &Path) -> Result<()> {
-    let buf_reader = BufReader::new(&mut stream);
-
-    let lines: Vec<_> = buf_reader
-        .lines()
-        .map(|line| line.unwrap())
-        // an empty line signifies the end of the HTTP request
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    let request = parse_request(&lines)?;
-
-    if cfg!(debug_assertions) {
-        println!("received request: {}", request);
-    }
-
-    let response = server::serve_file(&request, path)?;
-
-    if cfg!(debug_assertions) {
-        println!(
-            "response: {}, thread: {:#?}",
-            response.format_status_line(),
-            thread::current().id().as_u64()
-        );
-    }
-
-    stream.write_all(response.format().as_bytes())?;
-
-    Ok(())
-}
+use server::Server;
 
 fn main() -> Result<()> {
     let args = CLIArgs::parse();
     let file_serve_path = args.get_default_serve_dir();
 
-    let listener = TcpListener::bind(SERVER_ADDRESS)
-        .with_context(|| format!("Failed to bind to {}!", SERVER_ADDRESS))?;
-
-    if cfg!(debug_assertions) {
-        println!("main thread started, thread id: {}", thread::current().id().as_u64());
-    }
-
-    println!("Listening at http://{} 👂", SERVER_ADDRESS);
-
-    let mut thread_handles = vec![];
-
-    for stream in listener.incoming() {
-        let file_serve_path = file_serve_path.clone();
-        let handle = thread::spawn(move || -> Result<()> {
-            handle_stream(
-                stream.context("failed to make connection")?,
-                &file_serve_path,
-            )
-            .context("failed to handle request")?;
-            Ok(())
-        });
-        thread_handles.push(handle);
-    }
-
-    for handle in thread_handles.into_iter() {
-        handle.join().unwrap()?;
-    }
+    let server = Server::new(file_serve_path);
+    server.start()?;
 
     Ok(())
 }
---
+++ src/server/mod.rs
@@ -0,0 +1,88 @@
+use crate::parser::parse_request;
+use anyhow::{Context, Result};
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+use std::path::PathBuf;
+use std::thread;
+
+mod file;
+
+static SERVER_ADDRESS: &str = "127.0.0.1:8080";
+
+pub struct Server {
+    file_path: PathBuf,
+}
+
+impl Server {
+    pub fn new(file_path: PathBuf) -> Server {
+        Server { file_path }
+    }
+
+    pub fn start(self) -> Result<()> {
+        let listener =
+            TcpListener::bind(SERVER_ADDRESS).with_context(|| {
+                format!("Failed to bind to {}!", SERVER_ADDRESS)
+            })?;
+
+        if cfg!(debug_assertions) {
+            println!(
+                "main thread started, thread id: {}",
+                thread::current().id().as_u64()
+            );
+        }
+
+        println!("Listening at http://{} 👂", SERVER_ADDRESS);
+
+        let mut thread_handles = vec![];
+
+        for stream in listener.incoming() {
+            let file_serve_path = self.file_path.clone();
+            let handle = thread::spawn(move || -> Result<()> {
+                handle_stream(
+                    stream.context("failed to make connection")?,
+                    &file_serve_path,
+                )
+                .context("failed to handle request")?;
+                Ok(())
+            });
+            thread_handles.push(handle);
+        }
+
+        for handle in thread_handles.into_iter() {
+            handle.join().unwrap()?;
+        }
+        Ok(())
+    }
+}
+
+fn handle_stream(mut stream: TcpStream, path: &Path) -> Result<()> {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let lines: Vec<_> = buf_reader
+        .lines()
+        .map(|line| line.unwrap())
+        // an empty line signifies the end of the HTTP request
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let request = parse_request(&lines)?;
+
+    if cfg!(debug_assertions) {
+        println!("received request: {}", request);
+    }
+
+    let response = file::serve_file(&request, path)?;
+
+    if cfg!(debug_assertions) {
+        println!(
+            "response: {}, thread: {:#?}",
+            response.format_status_line(),
+            thread::current().id().as_u64()
+        );
+    }
+
+    stream.write_all(response.format().as_bytes())?;
+
+    Ok(())
+}
